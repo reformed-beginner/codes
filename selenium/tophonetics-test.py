b64b71a6f75c1bc6c5ff2fc385b12d26d28a887d
reformed-beginner <55189975+reformed-beginner@users.noreply.github.com>
2019-10-18 18:52:51 +0800

    Create tophonetics-test.py

---
+++ selenium/tophonetics-test.py
@@ -0,0 +1,42 @@
+#id
+#text_to_transcribe
+#clear_button
+#submit
+#transcr_output
+#weak_forms
+#preBracket
+#postBracket
+
+#name
+
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome('C:/Users/xb.teng.CORP/Downloads/chromedriver_win32/chromedriver.exe')
+browser.get('https://tophonetics.com/')
+#time.sleep(10)
+
+browser.find_element_by_id("text_to_transcribe").send_keys("Time is a versatile performer. It flies, marches on, heals all wounds, runs out and will tell.")
+browser.find_element_by_id('weak_forms').click()
+browser.find_element_by_id('preBracket').send_keys('[')
+browser.find_element_by_id('postBracket').send_keys(']')
+
+browser.find_element_by_xpath('//*[@id="options_div"]/label[1]/input').click()  #British Prononciation
+browser.find_element_by_id('submit').click()
+#time.sleep(10)
+
+text = browser.find_element_by_id('transcr_output').text
+print(text)
+
+#time.sleep(5)
+
+browser.find_element_by_xpath('//*[@id="options_div"]/label[2]/input').click()  #American Prononciation
+browser.find_element_by_id('submit').click()
+#time.sleep(10)
+
+text = browser.find_element_by_id('transcr_output').text
+print(text)
+time.sleep(5)
+
+print('Done')
+browser.quit()
